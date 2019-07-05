f2a08fec26acc750cb2aac3aa3e63836639a346c
tongxue <tongtongyuanyuan12@gmail.com>
2019-07-04 20:50:43 -0400

    Sort by rating with original id order.

--- BusinessInfo.cpp
+++ BusinessInfo.cpp
@@ -9,30 +9,68 @@
 
 using namespace std;
 
-struct BussinessInfo{
+//class BusinessInfo{
+//public:
+//
+//    int id;
+//    int rating;
+//    BusinessInfo(int id, int rating) : id(id), rating(rating) {}
+//
+//    friend bool operator < (const BusinessInfo& b1, const BusinessInfo& b2){
+//        return b1.rating > b2.rating;
+//    }
+//};
+
+struct BusinessInfo{
     int id;
     int rating;
-
-    BusinessInfo(int id, int rating) : id(id), rating(rating) {}
+    BusinessInfo(int id, int rating) : id(id), rating(rating){}
 };
 
 
 class Solution {
 public:
-    map<int, int> sortBusinessByRating (vector<BusinessInfo>& input) {
-        map<int,priority_queue<int,vector<int> ,greater<int>>> m;
-        for(auto& r : input) {
-            m[r.id].push(r.rating);
-        }
+    vector<BusinessInfo> sortBusinessByRating (vector<BusinessInfo>& input) {
+        auto cmp = [](BusinessInfo& b1, BusinessInfo& b2){
+            return b1.rating > b2.rating;
+        };
+        sort(input.begin(), input.end());
+        return input;
     }
+
+
 
 };
 
 int main() {
-    BusinessInfo b1["id" : 101, ratring : 5];
-    BusinessInfo b1["id" : 102, ratring : 2];
-    BusinessInfo b1["id" : 103, ratring : 3];
-    BusinessInfo b1["id" : 104, ratring : 5];
-    BusinessInfo b1["id" : 105, ratring : 5];
-    cout << sortBusinessByRating
+//    BusinessInfo b1("id" : 101, ratring : 5];
+//    BusinessInfo b1["id" : 102, ratring : 2];
+//    BusinessInfo b1["id" : 103, ratring : 3];
+//    BusinessInfo b1["id" : 104, ratring : 5];
+//    BusinessInfo b1["id" : 105, ratring : 5];
+    vector<BusinessInfo> input;
+    input.emplace_back(BusinessInfo(101, 5));
+    input.emplace_back(BusinessInfo(102, 2));
+    input.emplace_back(BusinessInfo(103, 3));
+    input.emplace_back(BusinessInfo(104, 5));
+    input.emplace_back(BusinessInfo(105, 5));
+
+    Solution s;
+    vector<BusinessInfo> output = s.sortBusinessByRating(input);
+
+    for(auto& o : output){
+        cout << o.id << ' ' << o.rating << endl;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
