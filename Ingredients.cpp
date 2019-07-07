87723abd3b1cf4db3a9b3c5c449eb5f491049135
tongxue <tongtongyuanyuan12@gmail.com>
2019-07-06 20:12:57 -0400

    finish find id = "restaruant" and "japanese"

--- Ingredients.cpp
+++ Ingredients.cpp
@@ -16,19 +16,19 @@ struct Meal
     string name; vector<string> ingredients;
 };
 
-vector<string> split(const string& str, const string& delim) {
-    vector<string> tokens;
-    int prev = 0, pos = 0;
-    do {
-        pos = str.find(delim, prev);
-        if (pos == string::npos) pos = str.length();
-        string token = str.substr(prev, pos-prev);
-        if (!token.empty()) tokens.push_back(token);
-        prev = pos + delim.length();
-    }
-    while (pos < str.length() && prev < str.length());
-    return tokens;
-}
+//vector<string> split(const string& str, const string& delim) {
+//    vector<string> tokens;
+//    int prev = 0, pos = 0;
+//    do {
+//        pos = str.find(delim, prev);
+//        if (pos == string::npos) pos = str.length();
+//        string token = str.substr(prev, pos-prev);
+//        if (!token.empty()) tokens.push_back(token);
+//        prev = pos + delim.length();
+//    }
+//    while (pos < str.length() && prev < str.length());
+//    return tokens;
+//}
 #include <unordered_set>
 
 
@@ -45,22 +45,22 @@ int getUniqueMealCount(const vector<Meal> meals) {
 int main() {
     string line;
     vector<Meal> meals;
-
-    while (true) {
-        getline(cin, line);
-        if (cin.fail()) {
-            cout << "A parsing error occurred";
-        }
-
-        vector<string> mealIngredients = split(line, " - ");
-        vector<string> ingredients = split(mealIngredients[1], ",");
-        string name = mealIngredients[0];
-        meals.push_back(Meal(name, ingredients));
-
-        if (cin.eof()) {
-            break;
-        }
-    }
+//
+//    while (true) {
+//        getline(cin, line);
+//        if (cin.fail()) {
+//            cout << "A parsing error occurred";
+//        }
+//
+//        vector<string> mealIngredients = split(line, " - ");
+//        vector<string> ingredients = split(mealIngredients[1], ",");
+//        string name = mealIngredients[0];
+//        meals.push_back(Meal(name, ingredients));
+//
+//        if (cin.eof()) {
+//            break;
+//        }
+//    }
     cout << getUniqueMealCount(meals) << endl;
     return 0;
 }
