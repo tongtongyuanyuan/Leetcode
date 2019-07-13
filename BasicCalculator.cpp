d71311c297da5d31be433f3585953f0b5629f666
tongxue <tongtongyuanyuan12@gmail.com>
2019-07-13 11:07:20 -0400

    finish find id = "restaruant" and "japanese"

--- BasicCalculator.cpp
+++ BasicCalculator.cpp
@@ -42,3 +42,30 @@ int main() {
     Solution S;
     cout << S.calculate(s) << "";
 }
+// class Solution{
+// public:
+//      int calculate(string s) {
+//      int num = 0, sign = 1, res = 0;
+//      stack<int>st;
+//     for(int i = 0; i < s.size(); i++) {
+//         char c = s[i];
+//         if(c >= '0') {
+//            num = 10 * num + c - '0';
+//         }
+//         else if(c == '+' || c== '-') {
+//             res += sign * num;
+//             num = 0; //每次初始化为0，以免改变结果，更新下一次结果
+//             sign = (c == '+') ? 1 : -1;
+//         } else if(c == '(') {
+//             st.push(num);
+//             num = 0;
+//         }else if(c == ')') {
+//             res += sign * num;
+//             st.push(res);
+//             num = 0;
+//         }
+//     }
+//     res += sign * num;
+//     return res;
+//  }
+// }; //runtime error
